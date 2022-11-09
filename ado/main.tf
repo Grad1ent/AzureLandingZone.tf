2d56210f0cdaaaa3dfdbe33054132ed229b249a3
Wojciech.Pazdzierkiewicz@kyndryl.com <Wojciech.Pazdzierkiewicz@kyndryl.com>
2022-11-09 15:53:55 +0100

    v.0.08: ado > dev

--- ado/main.tf
+++ ado/main.tf
@@ -6,28 +6,28 @@ module "main" {
     #
     # Hub
     #
-    rg_hub_name = "adoHub"
+    rg_hub_name = "devHub"
 
-    vnet_hub_name = "adoHubVnet"
+    vnet_hub_name = "devHubVnet"
     vnet_hub_address_space = ["10.100.0.0/16"]
 
     snet_hub_bastion_name = "AzureBastionSubnet"
     snet_hub_bastion_address_prefixes = ["10.100.10.0/24"]
-    nsg_snet_hub_bastion_name = "adoHubBastionSnetNsg"
+    nsg_snet_hub_bastion_name = "devHubBastionSnetNsg"
 
-    snet_hub_name = "adoHubSnet"
+    snet_hub_name = "devHubSnet"
     snet_hub_address_prefixes = ["10.100.100.0/24"]
-    nsg_snet_hub_name = "adoHubSnetNsg"
+    nsg_snet_hub_name = "devHubSnetNsg"
 
     #
     # Spoke 1
     #
-    rg_spoke_01_name = "adoSpoke01"
+    rg_spoke_01_name = "devSpoke01"
 
-    vnet_spoke_01_name = "adoSpoke01Vnet"
+    vnet_spoke_01_name = "devSpoke01Vnet"
     vnet_spoke_01_address_space = ["10.101.0.0/16"]
 
-    snet_spoke_01_name = "adoSpoke01Snet"
+    snet_spoke_01_name = "devSpoke01Snet"
     snet_spoke_01_address_prefixes = ["10.101.100.0/24"]
-    nsg_snet_spoke_01_name = "adoSpoke01SnetNsg"
+    nsg_snet_spoke_01_name = "devSpoke01SnetNsg"
 }
