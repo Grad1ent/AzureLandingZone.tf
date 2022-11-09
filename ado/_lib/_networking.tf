49c1be32e61e4f907e883aa8742e0e5a0e7a2f4b
Wojciech.Pazdzierkiewicz@kyndryl.com <Wojciech.Pazdzierkiewicz@kyndryl.com>
2022-11-09 12:23:31 +0100

    v.0.04: snet inside vnet

--- ado/_lib/_networking.tf
+++ ado/_lib/_networking.tf
@@ -6,20 +6,16 @@ resource "azurerm_virtual_network" "vnet_hub" {
   address_space       = var.vnet_hub_address_space
   location            = var.location
   resource_group_name = var.rg_hub_name
-}
 
-resource "azurerm_subnet" "snet_hub_bastion" {
-  name                 = var.snet_hub_bastion_name
-  virtual_network_name = var.vnet_hub_name
-  address_prefixes       = var.snet_hub_bastion_address_prefixes
-  resource_group_name  = var.rg_hub_name
-}
+  subnet{
+    name                 = var.snet_hub_bastion_name
+    address_prefix       = var.snet_hub_bastion_address_prefix
+  }
 
-resource "azurerm_subnet" "snet_hub" {
-  name                 = var.snet_hub_name
-  virtual_network_name = var.vnet_hub_name
-  address_prefixes       = var.snet_hub_address_prefixes
-  resource_group_name  = var.rg_hub_name
+  subnet {
+    name                 = var.snet_hub_name
+    address_prefix       = var.snet_hub_address_prefix
+  }
 }
 
 #
@@ -30,22 +26,9 @@ resource "azurerm_virtual_network" "vnet_spoke_01" {
   address_space       = var.vnet_spoke_01_address_space
   location            = var.location
   resource_group_name = var.rg_spoke_01_name
+
+  subnet {
+    name               = var.snet_spoke_01_name
+    address_prefix     = var.snet_spoke_01_address_prefix
+  }
 }
-
-resource "azurerm_subnet" "snet_spoke_01" {
-  name                 = var.snet_spoke_01_name
-  virtual_network_name = var.vnet_spoke_01_name
-  address_prefixes       = var.snet_spoke_01_address_prefixes
-  resource_group_name  = var.rg_spoke_01_name
-}
-
-
-
-
-
-
-
-
-
-
-
--- ado/main.tf
+++ ado/main.tf
@@ -12,10 +12,10 @@ module "main" {
     vnet_hub_address_space = ["10.100.0.0/16"]
 
     snet_hub_bastion_name = "AzureBastionSubnet"
-    snet_hub_bastion_address_prefixes = ["10.100.10.0/24"]
+    snet_hub_bastion_address_prefix = "10.100.10.0/24"
 
     snet_hub_name = "adoHubSnet"
-    snet_hub_address_prefixes = ["10.100.100.0/24"]
+    snet_hub_address_prefix = "10.100.100.0/24"
 
     #
     # Spoke 1
@@ -26,5 +26,5 @@ module "main" {
     vnet_spoke_01_address_space = ["10.101.0.0/16"]
 
     snet_spoke_01_name = "adoSpoke01Snet"
-    snet_spoke_01_address_prefixes = ["10.101.100.0/24"]
+    snet_spoke_01_address_prefix = "10.101.100.0/24"
 }
