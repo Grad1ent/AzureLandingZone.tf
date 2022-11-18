a82b987d16c53deae84d3685ffaa553ab32cdb64
Wojciech.Pazdzierkiewicz@kyndryl.com <Wojciech.Pazdzierkiewicz@kyndryl.com>
2022-11-18 12:57:50 +0100

    v.0.08: Bastion NSG updated

--- ado/_lib/_networking.tf
+++ ado/_lib/_networking.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "snet_hub" {
   name                 = var.snet_hub_name
   resource_group_name  = var.rg_hub_name
   virtual_network_name = var.vnet_hub_name
-  address_prefixes     = var.vnet_hub_address_space
+  address_prefixes     = var.snet_hub_address_prefixes
 }
 
 #
@@ -63,12 +63,107 @@ resource "azurerm_network_security_group" "nsg_snet_hub_bastion" {
   name                = var.nsg_snet_hub_bastion_name
   location            = var.location
   resource_group_name = var.rg_hub_name
+
+security_rule {
+    name                       = "AllowHttpsInbound"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+security_rule {
+    name                       = "AllowGatewayManagerInbound"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "GatewayManager"
+    destination_address_prefix = "*"
+  }
+security_rule {
+    name                       = "AllowAzureLoadBalancerInbound"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "AzureLoadBalancer"
+    destination_address_prefix = "*"
+  }
+security_rule {
+    name                       = "AllowBastionHostCommunication"
+    priority                   = 150
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "8080,5701"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+security_rule {
+    name                       = "AllowSshRdpOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "22,3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "VirtualNetwork"
+  }
+security_rule {
+    name                       = "AllowAzureCloudOutbound"
+    priority                   = 110
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "AzureCloud"
+  }
+security_rule {
+    name                       = "AllowBastionCommunication"
+    priority                   = 120
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "8080,5701"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+security_rule {
+    name                       = "AllowGetSessionInformation"
+    priority                   = 130
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "ass_nsg_snet_hub_bastion" {
   subnet_id                 = azurerm_subnet.snet_hub_bastion.id
   network_security_group_id = azurerm_network_security_group.nsg_snet_hub_bastion.id
 }
+
+
+
+
+
 
 resource "azurerm_network_security_group" "nsg_snet_hub" {
   name                = var.nsg_snet_hub_name
